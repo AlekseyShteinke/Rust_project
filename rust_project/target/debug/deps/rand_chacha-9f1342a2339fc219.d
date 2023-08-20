634c04a5197ae507cad465c37140f179a13d8f3a
AlekseyShteinke <43885149+AlekseyShteinke@users.noreply.github.com>
2023-08-20 23:19:16 +0300

    my commit

---
+++ rust_project/target/debug/deps/rand_chacha-9f1342a2339fc219.d
@@ -0,0 +1,9 @@
+C:\Users\Алекс\Desktop\Rust project\rust_project\target\debug\deps\rand_chacha-9f1342a2339fc219.rmeta: C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\Алекс\Desktop\Rust project\rust_project\target\debug\deps\librand_chacha-9f1342a2339fc219.rlib: C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\Алекс\Desktop\Rust project\rust_project\target\debug\deps\rand_chacha-9f1342a2339fc219.d: C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs:
+C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs:
+C:\Users\Алекс\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs:
---
+++ rust_project/target/debug/rust_project.d
@@ -0,0 +1,1 @@
+C:\Users\Алекс\Desktop\Rust\ project\rust_project\target\debug\rust_project.exe: C:\Users\Алекс\Desktop\Rust\ project\rust_project\src\main.rs
